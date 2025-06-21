81559fc77f695dba1a39a0c113067d53d08b39cd
seehiong <seehiong@gmail.com>
2025-06-22 00:52:56 +0800

    removed unneeded feature folder

--- src/features/review/utils/reviewTimeUtils.ts
+++
@@ -1,7 +0,0 @@
-// src/features/review/utils/reviewTimeUtils.ts
-export const formatDuration = (seconds: number): string => {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-  const s = seconds % 60;
-  return [h > 0 ? `${h}h` : '', m > 0 ? `${m}m` : '', `${s}s`].filter(Boolean).join(' ');
-};
